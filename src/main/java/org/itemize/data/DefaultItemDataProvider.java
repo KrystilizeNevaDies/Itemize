769bbad2912b8f7900399f62a8a18d1362314457
KrystilizeNevaDies <57762380+KrystilizeNevaDies@users.noreply.github.com>
2021-06-15 12:53:18 +1000

    Update DefaultItemDataProvider.java

--- src/main/java/org/itemize/data/DefaultItemDataProvider.java
+++ src/main/java/org/itemize/data/DefaultItemDataProvider.java
@@ -21,7 +21,7 @@ public class DefaultItemDataProvider implements ItemDataProvider {
 		TEST_BACK("Test Backpack", ItemType.BACK, ItemRarity.UNCOMMON, "This is a test backpack! Used for wearing and testing! Put it on your back slot!", 31),
 		TEST_ITEM("Test Item", ItemType.ITEM, ItemRarity.RARE, "This is a test item! Used for holding and testing!", 4),
 		TEST_HAT("Test Hat", ItemType.HAT, ItemRarity.EXOTIC, "This is a test hat! Used for wearing and testing!", 1),
-		TEST_COIN("Test Coin 1x", ItemType.CURRENCY, ItemRarity.MYTHICAL, "This is a test coin! Put it in your bank! You Sussy Baka", 55),
+		TEST_COIN("Test Coin 1x", ItemType.CURRENCY, ItemRarity.MYTHICAL, "This is a test coin! Put it in your bank!", 55),
 		TEST_BADGE("Test Badge", ItemType.BADGE, ItemRarity.LIMITED_EDITION, "This is a test badge! Put it in your badges slot on your player profile!", 2);
 
 		String ID;
